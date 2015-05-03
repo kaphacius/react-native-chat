293dc6ed9e05e56ffb441155c8a8f09f534b8154
kap <yurii.zadoianchuk@gmail.com>
2015-05-03 17:51:26 +0200

    adds code to move input when keyboard is open

--- RNChat/iOS/ChatView.swift
+++ RNChat/iOS/ChatView.swift
@@ -8,11 +8,12 @@
 
 import UIKit
 
-class ChatView: RCTView {
+class ChatView: UIView, UITextViewDelegate {
     var chatCollectionView: UICollectionView!
     var inputToolbar: UIToolbar!
     var inputTextView: UITextView!
     var sendButton: UIButton!
+    var inputToolBarBottomConstraint: NSLayoutConstraint!
     
     let kToolbarHeight: CGFloat = 44.0
     
@@ -26,6 +27,7 @@ class ChatView: RCTView {
         self.addSubview(inputToolbar)
         inputTextView = UITextView(frame: CGRectZero)
         inputTextView.setTranslatesAutoresizingMaskIntoConstraints(false)
+        inputTextView.delegate = self
         self.inputToolbar.addSubview(inputTextView)
         sendButton = UIButton(frame: CGRectZero)
         sendButton.setTranslatesAutoresizingMaskIntoConstraints(false)
@@ -37,8 +39,8 @@ class ChatView: RCTView {
         var inputToolbarHeight = NSLayoutConstraint(item: inputToolbar, attribute: NSLayoutAttribute.Height, relatedBy: NSLayoutRelation.Equal, toItem: nil, attribute: NSLayoutAttribute.NotAnAttribute, multiplier: 1.0, constant: kToolbarHeight)
         var inputToolbarLeading = NSLayoutConstraint(item: inputToolbar, attribute: NSLayoutAttribute.Leading, relatedBy: NSLayoutRelation.Equal, toItem: self, attribute: NSLayoutAttribute.Left, multiplier: 1.0, constant: 0.0)
         var inputToolbarTrailing = NSLayoutConstraint(item: inputToolbar, attribute: NSLayoutAttribute.Trailing, relatedBy: NSLayoutRelation.Equal, toItem: self, attribute: NSLayoutAttribute.Right, multiplier: 1.0, constant: 0.0)
-        var inputToolbarBottom = NSLayoutConstraint(item: inputToolbar, attribute: NSLayoutAttribute.Bottom, relatedBy: NSLayoutRelation.Equal, toItem: self, attribute: NSLayoutAttribute.Bottom, multiplier: 1.0, constant: 0.0)
-        self.addConstraints([inputToolbarLeading, inputToolbarTrailing, inputToolbarBottom])
+        inputToolBarBottomConstraint = NSLayoutConstraint(item: inputToolbar, attribute: NSLayoutAttribute.Bottom, relatedBy: NSLayoutRelation.Equal, toItem: self, attribute: NSLayoutAttribute.Bottom, multiplier: 1.0, constant: 0.0)
+        self.addConstraints([inputToolbarLeading, inputToolbarTrailing, inputToolBarBottomConstraint])
         inputToolbar.addConstraint(inputToolbarHeight)
         
         var collectionViewTop = NSLayoutConstraint(item: chatCollectionView, attribute: NSLayoutAttribute.Top, relatedBy: NSLayoutRelation.Equal, toItem: self, attribute: NSLayoutAttribute.Top, multiplier: 1.0, constant: 0.0);
@@ -55,22 +57,51 @@ class ChatView: RCTView {
         inputToolbar.addConstraints([sendButtonTop, sendButtonBottom, sendButtonTrailing])
         sendButton.addConstraint(sendButtonWidth)
         
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillShow:", name: UIKeyboardWillShowNotification, object: nil)
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillShow:", name: UIKeyboardWillHideNotification, object: nil)
+        var inputTextViewTop = NSLayoutConstraint(item: inputTextView, attribute: NSLayoutAttribute.Top, relatedBy: NSLayoutRelation.Equal, toItem: inputToolbar, attribute: NSLayoutAttribute.Top, multiplier: 1.0, constant: 0.0)
+        var inputTextViewBottom = NSLayoutConstraint(item: inputTextView, attribute: NSLayoutAttribute.Bottom, relatedBy: NSLayoutRelation.Equal, toItem: inputToolbar, attribute: NSLayoutAttribute.Bottom, multiplier: 1.0, constant: 0.0)
+        var inputTextViewLeading = NSLayoutConstraint(item: inputTextView, attribute: NSLayoutAttribute.Leading, relatedBy: NSLayoutRelation.Equal, toItem: inputToolbar, attribute: NSLayoutAttribute.Left, multiplier: 1.0, constant: 0.0)
+        var inputTextViewTrailing = NSLayoutConstraint(item: inputTextView, attribute: NSLayoutAttribute.Trailing, relatedBy: NSLayoutRelation.Equal, toItem: sendButton, attribute: NSLayoutAttribute.Left, multiplier: 1.0, constant: 0.0)
+        inputToolbar.addConstraints([inputTextViewTop, inputTextViewBottom, inputTextViewLeading, inputTextViewTrailing])
+        
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillChangeFrame:", name: UIKeyboardWillChangeFrameNotification, object: nil)
+        
+        sendButton.addTarget(self, action: "sendButtonTapped", forControlEvents: UIControlEvents.TouchUpInside)
     }
-
+    
     required init(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    func keyboardWillShow(nofitication: NSNotification) {
-
-    }
-    func keyboardWillHide(notification: NSNotification) {
-        
-    }
     func keyboardWillChangeFrame(notification: NSNotification) {
-        
+        if let ui = notification.userInfo,
+            let frameBeginValue = (ui[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.CGRectValue(),
+            let frameEndValue = (ui[UIKeyboardFrameEndUserInfoKey] as? NSValue)?.CGRectValue(),
+            let duration = (ui[UIKeyboardAnimationDurationUserInfoKey] as? NSNumber)?.doubleValue,
+            let curveInt = (ui[UIKeyboardAnimationCurveUserInfoKey] as? NSNumber)?.intValue,
+            let curve = UIViewAnimationCurve(rawValue: Int(curveInt)) {
+                self .animateInputToolbarWithYPosDiff(frameBeginValue.origin.y - frameEndValue.origin.y, duration: duration, animationCurve: curve)
+        }
+    }
+    
+    func sendButtonTapped() {
+        if true == inputTextView.isFirstResponder() {
+            inputTextView.resignFirstResponder()
+        }
+    }
+    
+    func textView(textView: UITextView, shouldChangeTextInRange range: NSRange, replacementText text: String) -> Bool {
+        if text == "\n" {
+            textView.resignFirstResponder()
+            return false
+        }
+        return true
+    }
+    
+    func animateInputToolbarWithYPosDiff(yPosDiff: CGFloat, duration: Double, animationCurve: UIViewAnimationCurve) {
+        var option = UInt(animationCurve.rawValue << 16)
+        UIView.animateWithDuration(duration, delay: 0.0, options: UIViewAnimationOptions(option), animations: { () -> Void in
+            self.inputToolBarBottomConstraint.constant -= yPosDiff
+            self.layoutIfNeeded()
+            }, completion: nil)
     }
 }
