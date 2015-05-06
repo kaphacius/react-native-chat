bfb1cc17f3eaabc1cc6239a7e7e5343faf6bcf60
kap <yurii.zadoianchuk@gmail.com>
2015-05-06 23:05:59 +0200

    adds file for chat message cell

---
+++ RNChat/ChatMessageCell.swift
@@ -0,0 +1,13 @@
+//
+//  ChatMessageCell.swift
+//  RNChat
+//
+//  Created by Yurii Zadoianchuk on 06/05/15.
+//  Copyright (c) 2015 Facebook. All rights reserved.
+//
+
+import UIKit
+
+class ChatMesageCell: UICollectionViewCell {
+  
+}
